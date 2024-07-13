75797365d4f42d753c5c544977b85c8a6cb37e31
Mathsx2 <laabc155@gmail.com>
2024-07-12 22:44:27 -0300

    Operações Básicas List

---
+++ src/list/OperacoesBasica/Tarefa.java
@@ -0,0 +1,14 @@
+package list.OperacoesBasica;
+
+public class Tarefa {
+    // Atributo
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
