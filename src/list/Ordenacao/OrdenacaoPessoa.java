fce5d98a51d9f50cf07fb3c623f8685fb2972d39
Mathsx2 <laabc155@gmail.com>
2024-07-15 13:50:20 -0300

    mudança import

--- src/list/Ordenacao/OrdenacaoPessoa.java
+++ src/list/Ordenacao/OrdenacaoPessoa.java
@@ -3,7 +3,7 @@ package list.Ordenacao;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Comparator;
+//import java.util.Comparator;
 
 public class OrdenacaoPessoa {
     //atributo
